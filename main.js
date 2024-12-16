b3710b451a70cfe27f0e5ed852ddb011ccb92fdf
Nico <52355359+NicoGBuch@users.noreply.github.com>
2024-12-16 09:50:27 +0100

    baller

--- main.js
+++ main.js
@@ -3,22 +3,39 @@ const gc = document.querySelector("#gameCanvas");
 const cwidth = gc.width;
 const cheight = gc.height;
 
+let balls = [];
+
 function draw() {
+
     ctx.drawImage(table, 0, 0, cwidth, cheight);
 
-    ctx.beginPath();
-    ctx.arc(0, 0, 40, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#003300';
-    ctx.stroke();
+    for (var i = 0; i < balls.length; i++) {
+
+        balls[i]["x"] += 1;
+        balls[i]["y"] += 1;
+
+        ctx.beginPath();
+        ctx.arc(balls[i]["x"], balls[i]["y"], 40, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+    }
+
+    window.requestAnimationFrame(draw);
 }
 
 if (gc.getContext) {
     var ctx = gc.getContext("2d");
     var table = new Image();
     table.src = "table.jpg";
+
+    // for (var i = 0; i < 32; i++) {
+    //     balls.push((0, 50), (300, 20));        
+    // }
+    balls.push({"x": 20, "y": 50});
+    balls.push({"x": 300, "y": 60});
 
     table.addEventListener("load", () => {
         draw();
